17721552f70feb114c86a02386473594c6a059e0
John Chrosniak <chrosniakj@gmail.com>
2024-06-24 12:26:25 -0400

    ported port drayage config from carma platform

---
+++ carma_nav2_port_drayage_demo/include/carma_nav2_port_drayage_demo/port_drayage_demo_config.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+/*
+ * Copyright (C) 2022 LEIDOS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+
+ * NOTE: ported from CARMA Platform's port_drayage_plugin package (port_drayage_plugin_config.hpp)
+ */
+
+#include <iostream>
+#include <vector>
+
+namespace carma_nav2_port_drayage_demo
+{
+
+  /**
+   * \brief Struct containing the algorithm configuration values for carma_nav2_port_drayage_demo
+   */
+  struct Config
+  {
+    std::string cmv_id = ""; // The CMV's static ID, which is its license plate
+
+    std::string cargo_id = ""; // The ID of the cargo that is being carried at system start-up. Empty if CMV is not carrying cargo.
+
+    bool enable_port_drayage = false; // Flag to activate/deactivate port drayage operations. If false, the port drayage state machine will remain INACTIVE
+
+    bool starting_at_staging_area = true; // Flag to indicate CMV's first destination; 'true' indicates Staging Area Entrance, 'false' indicates Port Entrance
+
+    // Stream operator for this config
+    friend std::ostream &operator<<(std::ostream &output, const Config &c)
+    {
+      output << "carma_nav2_port_drayage_demo::Config { " << std::endl
+           << "cmv_id: " << c.cmv_id << std::endl
+           << "cargo_id: " << c.cargo_id << std::endl
+           << "enable_port_drayage: " << c.enable_port_drayage << std::endl
+           << "starting_at_staging_area: " << c.starting_at_staging_area << std::endl
+           << "}" << std::endl;
+      return output;
+    }
+  };
+
+} // carma_nav2_port_drayage_demo
