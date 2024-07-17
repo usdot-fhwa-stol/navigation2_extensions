2f342d13a72910e0cc3bf2186d4785c211a75e5b
John Chrosniak <chrosniakj@gmail.com>
2024-07-17 14:01:35 -0400

    incorporated unit test from similar action nav2_msgs/action/ComputePathToPose

--- carma_nav2_behavior_tree/test/plugins/action/test_compute_route_action.cpp
+++ carma_nav2_behavior_tree/test/plugins/action/test_compute_route_action.cpp
@@ -107,131 +107,125 @@ std::shared_ptr<BT::Tree> ComputeRouteActionTestFixture::tree_ = nullptr;
 
 TEST_F(ComputeRouteActionTestFixture, test_tick)
 {
-  EXPECT_EQ(2, 1 + 1);
-  //   // create tree
-  //   std::string xml_txt =
-  //     R"(
-  //       <root BTCPP_format="4">
-  //         <BehaviorTree ID="MainTree">
-  //             <ComputeRoute goal="{goal}" path="{path}" planner_id="GridBased"/>
-  //         </BehaviorTree>
-  //       </root>)";
+  // create tree
+  std::string xml_txt =
+    R"(
+        <root BTCPP_format="4">
+          <BehaviorTree ID="MainTree">
+              <ComputeRoute goal="{goal}" path="{path}"/>
+          </BehaviorTree>
+        </root>)";
 
-  //   tree_ = std::make_shared<BT::Tree>(factory_->createTreeFromText(xml_txt, config_->blackboard));
+  tree_ = std::make_shared<BT::Tree>(factory_->createTreeFromText(xml_txt, config_->blackboard));
 
-  //   // create new goal and set it on blackboard
-  //   geometry_msgs::msg::PoseStamped goal;
-  //   goal.header.stamp = node_->now();
-  //   goal.pose.position.x = 1.0;
-  //   config_->blackboard->set("goal", goal);
+  // create new goal and set it on blackboard
+  geometry_msgs::msg::PoseStamped goal;
+  goal.header.stamp = node_->now();
+  goal.pose.position.x = 1.0;
+  config_->blackboard->set("goal", goal);
 
-  //   // tick until node succeeds
-  //   while (tree_->rootNode()->status() != BT::NodeStatus::SUCCESS) {
-  //     tree_->rootNode()->executeTick();
-  //   }
+  // tick until node succeeds
+  while (tree_->rootNode()->status() != BT::NodeStatus::SUCCESS) {
+    tree_->rootNode()->executeTick();
+  }
 
-  //   // the goal should have reached our server
-  //   EXPECT_EQ(tree_->rootNode()->status(), BT::NodeStatus::SUCCESS);
-  //   EXPECT_EQ(tree_->rootNode()->getInput<std::string>("planner_id"), std::string("GridBased"));
-  //   EXPECT_EQ(action_server_->getCurrentGoal()->goal.pose.position.x, 1.0);
-  //   EXPECT_FALSE(action_server_->getCurrentGoal()->use_start);
-  //   EXPECT_EQ(action_server_->getCurrentGoal()->planner_id, std::string("GridBased"));
+  // the goal should have reached our server
+  EXPECT_EQ(tree_->rootNode()->status(), BT::NodeStatus::SUCCESS);
+  EXPECT_EQ(action_server_->getCurrentGoal()->goal.pose.position.x, 1.0);
+  EXPECT_FALSE(action_server_->getCurrentGoal()->use_start);
 
-  //   // check if returned path is correct
-  //   nav_msgs::msg::Path path;
-  //   EXPECT_TRUE(config_->blackboard->get<nav_msgs::msg::Path>("path", path));
-  //   EXPECT_EQ(path.poses.size(), 2u);
-  //   EXPECT_EQ(path.poses[0].pose.position.x, 0.0);
-  //   EXPECT_EQ(path.poses[1].pose.position.x, 1.0);
+  // check if returned path is correct
+  nav_msgs::msg::Path path;
+  EXPECT_TRUE(config_->blackboard->get<nav_msgs::msg::Path>("path", path));
+  EXPECT_EQ(path.poses.size(), 2u);
+  EXPECT_EQ(path.poses[0].pose.position.x, 0.0);
+  EXPECT_EQ(path.poses[1].pose.position.x, 1.0);
 
-  //   // halt node so another goal can be sent
-  //   tree_->haltTree();
-  //   EXPECT_EQ(tree_->rootNode()->status(), BT::NodeStatus::IDLE);
+  // halt node so another goal can be sent
+  tree_->haltTree();
+  EXPECT_EQ(tree_->rootNode()->status(), BT::NodeStatus::IDLE);
 
-  //   // set new goal
-  //   goal.pose.position.x = -2.5;
-  //   config_->blackboard->set("goal", goal);
+  // set new goal
+  goal.pose.position.x = -2.5;
+  config_->blackboard->set("goal", goal);
 
-  //   while (tree_->rootNode()->status() != BT::NodeStatus::SUCCESS) {
-  //     tree_->rootNode()->executeTick();
-  //   }
+  while (tree_->rootNode()->status() != BT::NodeStatus::SUCCESS) {
+    tree_->rootNode()->executeTick();
+  }
 
-  //   EXPECT_EQ(tree_->rootNode()->status(), BT::NodeStatus::SUCCESS);
-  //   EXPECT_EQ(action_server_->getCurrentGoal()->goal.pose.position.x, -2.5);
+  EXPECT_EQ(tree_->rootNode()->status(), BT::NodeStatus::SUCCESS);
+  EXPECT_EQ(action_server_->getCurrentGoal()->goal.pose.position.x, -2.5);
 
-  //   EXPECT_TRUE(config_->blackboard->get<nav_msgs::msg::Path>("path", path));
-  //   EXPECT_EQ(path.poses.size(), 2u);
-  //   EXPECT_EQ(path.poses[0].pose.position.x, 0.0);
-  //   EXPECT_EQ(path.poses[1].pose.position.x, -2.5);
+  EXPECT_TRUE(config_->blackboard->get<nav_msgs::msg::Path>("path", path));
+  EXPECT_EQ(path.poses.size(), 2u);
+  EXPECT_EQ(path.poses[0].pose.position.x, 0.0);
+  EXPECT_EQ(path.poses[1].pose.position.x, -2.5);
 }
 
 TEST_F(ComputeRouteActionTestFixture, test_tick_use_start)
 {
-  EXPECT_EQ(2, 1 + 1);
-  //   // create tree
-  //   std::string xml_txt =
-  //     R"(
-  //       <root BTCPP_format="4">
-  //         <BehaviorTree ID="MainTree">
-  //             <ComputeRoute goal="{goal}" start="{start}" path="{path}" planner_id="GridBased"/>
-  //         </BehaviorTree>
-  //       </root>)";
+  // create tree
+  std::string xml_txt =
+    R"(
+        <root BTCPP_format="4">
+          <BehaviorTree ID="MainTree">
+              <ComputeRoute goal="{goal}" start="{start}" path="{path}"/>
+          </BehaviorTree>
+        </root>)";
 
-  //   tree_ = std::make_shared<BT::Tree>(factory_->createTreeFromText(xml_txt, config_->blackboard));
+  tree_ = std::make_shared<BT::Tree>(factory_->createTreeFromText(xml_txt, config_->blackboard));
 
-  //   // create new start and set it on blackboard
-  //   geometry_msgs::msg::PoseStamped start;
-  //   start.header.stamp = node_->now();
-  //   start.pose.position.x = 2.0;
-  //   config_->blackboard->set("start", start);
+  // create new start and set it on blackboard
+  geometry_msgs::msg::PoseStamped start;
+  start.header.stamp = node_->now();
+  start.pose.position.x = 2.0;
+  config_->blackboard->set("start", start);
 
-  //   // create new goal and set it on blackboard
-  //   geometry_msgs::msg::PoseStamped goal;
-  //   goal.header.stamp = node_->now();
-  //   goal.pose.position.x = 1.0;
-  //   config_->blackboard->set("goal", goal);
+  // create new goal and set it on blackboard
+  geometry_msgs::msg::PoseStamped goal;
+  goal.header.stamp = node_->now();
+  goal.pose.position.x = 1.0;
+  config_->blackboard->set("goal", goal);
 
-  //   // tick until node succeeds
-  //   while (tree_->rootNode()->status() != BT::NodeStatus::SUCCESS) {
-  //     tree_->rootNode()->executeTick();
-  //   }
+  // tick until node succeeds
+  while (tree_->rootNode()->status() != BT::NodeStatus::SUCCESS) {
+    tree_->rootNode()->executeTick();
+  }
 
-  //   // the goal should have reached our server
-  //   EXPECT_EQ(tree_->rootNode()->status(), BT::NodeStatus::SUCCESS);
-  //   EXPECT_EQ(tree_->rootNode()->getInput<std::string>("planner_id"), std::string("GridBased"));
-  //   EXPECT_EQ(action_server_->getCurrentGoal()->goal.pose.position.x, 1.0);
-  //   EXPECT_EQ(action_server_->getCurrentGoal()->start.pose.position.x, 2.0);
-  //   EXPECT_TRUE(action_server_->getCurrentGoal()->use_start);
-  //   EXPECT_EQ(action_server_->getCurrentGoal()->planner_id, std::string("GridBased"));
+  // the goal should have reached our server
+  EXPECT_EQ(tree_->rootNode()->status(), BT::NodeStatus::SUCCESS);
+  EXPECT_EQ(action_server_->getCurrentGoal()->goal.pose.position.x, 1.0);
+  EXPECT_EQ(action_server_->getCurrentGoal()->start.pose.position.x, 2.0);
+  EXPECT_TRUE(action_server_->getCurrentGoal()->use_start);
 
-  //   // check if returned path is correct
-  //   nav_msgs::msg::Path path;
-  //   EXPECT_TRUE(config_->blackboard->get<nav_msgs::msg::Path>("path", path));
-  //   EXPECT_EQ(path.poses.size(), 2u);
-  //   EXPECT_EQ(path.poses[0].pose.position.x, 2.0);
-  //   EXPECT_EQ(path.poses[1].pose.position.x, 1.0);
+  // check if returned path is correct
+  nav_msgs::msg::Path path;
+  EXPECT_TRUE(config_->blackboard->get<nav_msgs::msg::Path>("path", path));
+  EXPECT_EQ(path.poses.size(), 2u);
+  EXPECT_EQ(path.poses[0].pose.position.x, 2.0);
+  EXPECT_EQ(path.poses[1].pose.position.x, 1.0);
 
-  //   // halt node so another goal can be sent
-  //   tree_->haltTree();
-  //   EXPECT_EQ(tree_->rootNode()->status(), BT::NodeStatus::IDLE);
+  // halt node so another goal can be sent
+  tree_->haltTree();
+  EXPECT_EQ(tree_->rootNode()->status(), BT::NodeStatus::IDLE);
 
-  //   // set new goal and new start
-  //   goal.pose.position.x = -2.5;
-  //   start.pose.position.x = -1.5;
-  //   config_->blackboard->set("goal", goal);
-  //   config_->blackboard->set("start", start);
+  // set new goal and new start
+  goal.pose.position.x = -2.5;
+  start.pose.position.x = -1.5;
+  config_->blackboard->set("goal", goal);
+  config_->blackboard->set("start", start);
 
-  //   while (tree_->rootNode()->status() != BT::NodeStatus::SUCCESS) {
-  //     tree_->rootNode()->executeTick();
-  //   }
+  while (tree_->rootNode()->status() != BT::NodeStatus::SUCCESS) {
+    tree_->rootNode()->executeTick();
+  }
 
-  //   EXPECT_EQ(tree_->rootNode()->status(), BT::NodeStatus::SUCCESS);
-  //   EXPECT_EQ(action_server_->getCurrentGoal()->goal.pose.position.x, -2.5);
+  EXPECT_EQ(tree_->rootNode()->status(), BT::NodeStatus::SUCCESS);
+  EXPECT_EQ(action_server_->getCurrentGoal()->goal.pose.position.x, -2.5);
 
-  //   EXPECT_TRUE(config_->blackboard->get<nav_msgs::msg::Path>("path", path));
-  //   EXPECT_EQ(path.poses.size(), 2u);
-  //   EXPECT_EQ(path.poses[0].pose.position.x, -1.5);
-  //   EXPECT_EQ(path.poses[1].pose.position.x, -2.5);
+  EXPECT_TRUE(config_->blackboard->get<nav_msgs::msg::Path>("path", path));
+  EXPECT_EQ(path.poses.size(), 2u);
+  EXPECT_EQ(path.poses[0].pose.position.x, -1.5);
+  EXPECT_EQ(path.poses[1].pose.position.x, -2.5);
 }
 
 int main(int argc, char ** argv)
