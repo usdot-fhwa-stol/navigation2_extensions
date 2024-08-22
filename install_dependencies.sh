8b8986944c3035f2da9c9e5b3b72b1e52b97c37b
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2024-08-21 23:22:26 -0400

    update ros deps

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,4 +4,4 @@ sudo apt-get update
 # Install 
 apt install -y libnanoflann-dev
 rosdep update
-rosdep install --from-paths /__w/navigation2_extensions/navigation2_extensions/src --ignore-src --rosdistro=humble -y -r
+rosdep install --from-paths src --ignore-src --rosdistro=humble -y -r
