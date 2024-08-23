f9fc9eb54ed269a577f55a02032ad56173f33f61
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2024-08-23 11:04:20 -0400

    fix

--- docker/install_dependencies.sh
+++ docker/install_dependencies.sh
@@ -3,9 +3,6 @@ set -e
 sudo apt-get update
 # Install 
 apt install -y libnanoflann-dev
-# Set the working directory
-cd /root/c1t_ws/src/
-git clone https://github.com/usdot-fhwa-stol/carma-msgs
-git clone -b nav2_route_server_humble https://github.com/usdot-fhwa-stol/navigation2
+git clone https://github.com/usdot-fhwa-stol/carma-msgs /root/c1t_ws/src/carma-msgs && git clone -b nav2_route_server_humble https://github.com/usdot-fhwa-stol/navigation2 /root/c1t_ws/src/navigation2
 rosdep update
 rosdep install --from-paths src --ignore-src --rosdistro=humble -y -r
